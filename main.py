5fad111dfd04b2c4592c1c0ab1415c7dd24ec295
vixokiller <vixokiller@gmail.com>
2026-06-19 21:57:26 -0400

    Jugar cartas

--- main.py
+++ main.py
@@ -7,18 +7,46 @@ class Card:
         return self.name
         
 class Ally(Card):
-    def __init__(self,name, cost, strenght):
+    def __init__(self, name, cost, strenght):
         super().__init__(name, cost)
         self.strenght = strenght
-        
-alido1 = Ally("Zeus", 1, 2)
-oro1 = Card("Copihue")
-hand = [alido1, oro1]
-table = []
 
+class Gold(Card):
+    def __init__(self, name, cost=None):
+        super().__init__(name, cost)
+       
+def show_cards(zone):
+    for card in zone:
+        print(card)
 
-if not oro1.cost:
-    hand.remove(oro1)
-    table.append(oro1)
+def paid(gold_reserve, paid_gold_zone, card):
+    for _ in range(card.cost):
+        gold = gold_reserve.pop()
+        paid_gold_zone.append(gold)
+    show_cards(paid_gold_zone)
     
+def play_card(hand, zone, card):
+    if isinstance(card, Ally):
+        hand.remove(card)
+        zone.append(card)
+        show_cards(zone)
+        
+    elif isinstance(card, Gold):
+        hand.remove(card)
+        zone.append(card)
+        show_cards(zone)
+         
+aliado1 = Ally("Zeus", 2, 2)
+oro1 = Gold("Copihue")
+oro2 = Gold("Aguila imperial")
+gold_reserve = []
+paid_gold_zone = []
+hand = [aliado1, oro1, oro2]
+defense_line = []
+play_card(hand, gold_reserve, oro1)
+play_card(hand, gold_reserve, oro2)
+paid(gold_reserve, paid_gold_zone, aliado1)
+
+
+
 
