3549f3ed85c264795ea85ab8df8dbcc4ca42f789
vixokiller <vixokiller@gmail.com>
2026-06-22 16:55:33 -0400

    Fases #2

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import random
 
 class Card:
+    '''
+    Tipos de carta:
+    Oro = 0
+    Aliado = 1
+    Arma = 2
+    Tótem = 3
+    Talismán = 4
+    '''
     def __init__(self, name, type, cost=None):
         self.name = name
         self.type = type
         self.cost = cost
+        self.controller = None
         
     def __repr__(self):
         return self.name
@@ -13,6 +22,9 @@ class Ally(Card):
     def __init__(self, name, type, cost, strenght):
         super().__init__(name, type, cost)
         self.strenght = strenght
+        self.enter_turn = 0
+        self.can_attack = True
+        self.can_defense = True
 
 class Gold(Card):
     def __init__(self, name, type, cost=None):
@@ -113,7 +125,7 @@ class Game:
     Turno
         Agrupación = 0
         Vigilia = 1
-        Batalla Mitológica
+        Batalla Mitológica:
             Declaración de Ataque = 2
             Declaración de Bloqueo = 3
             Guerra de Talismanes = 4
@@ -126,6 +138,8 @@ class Game:
         self.actual_turn = 1
         self.actual_phase = 0
         self.active_player_index = 0
+        self.some_card_play_vigil = False
+        self.locks = {}
         
     @property
     def active_player(self):
@@ -139,14 +153,74 @@ class Game:
         player = self.active_player
         player.attack_line.regroup(player.defense_line)
         player.paid_reserve_gold.regroup(player.gold_reserve)
+        
+    def play_card_hand(self, card):
+        player = self.active_player
+        if card.type == 0:
+            if self.some_card_play_vigil:
+                print("Ya jugaste tú primera carta.")
+                return False
+            player.hand.move_to(card, player.gold_reserve)
+            self.some_card_play_vigil = True
+            return True
+        if card.type == 1:
+            player.hand.move_to(card, player.defense_line)
+            card.enter_turn = self.actual_turn
+            return True
+        return False
     
-aliado1 = Ally("Zeus", "Aliado", 3, 2)
-aliado2 = Ally("Apolo", "Aliado", 1, 1)
-oro1 = Gold("Copihue", "Oro")
-oro2 = Gold("Aguila imperial", "Oro")
+    def statement_attack(self, allys):
+        player = self.active_player
+        for ally in allys:
+            if ally.enter_turn == self.actual_turn:
+                print("Este aliado no puede atacar por qué entro este turno.")
+                continue
+            if ally.can_attack == False:
+                print(f"{ally.name} no puede atacar.")
+                continue
+            player.defense_line.move_to(ally, player.attack_line)
+        
+    def declare_blocks(self, locks):
+        defender = self.defender_player
+        player = self.active_player
+        for attacker, blocker in locks:
+            if attacker not in player.attack_line.cards:
+                print(f"{attacker.name} no esta atacando.")
+                return False
+            if blocker not in defender.defense_line.cards:
+                print(f"{blocker.name} no esta en la Línea de defensa.")
+                return False
+            if blocker.can_defense == False:
+                print(f"{blocker.name} no puede defender.")
+                return False
+        self.locks = locks
+        return True
+    
+    def detroy_card(self, card):
+        player = card.cotroller
+        zones = [player.attack_line, player.defense_line, player.support_line]
+        for zone in zones:
+            if card in zone.cards:
+                zone.move_to(card, player.graveyard)
+        
+    def damage_assignment(self):
+        attacker = self.active_player
+        defender = self.defender_player
+            
+        
+            
+    
+aliado1 = Ally("Zeus", 1, 3, 2)
+aliado2 = Ally("Apolo", 1, 1, 1)
+oro1 = Gold("Copihue", 0)
+oro2 = Gold("Aguila imperial", 0)
 jugador1 = Player("Vicente")
 jugador2 = Player("Gabriel")
 partida = Game(jugador1, jugador2)
+
+
+
+
 
 
 
