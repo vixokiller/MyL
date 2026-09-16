a0aaf5dbeb06826744b34cacc28a442ea2c85417
vixokiller <vixokiller@gmail.com>
2026-09-16 16:54:35 -0300

    Reinicio

--- load_cards.py
+++
@@ -1,6 +0,0 @@
-from database import create_tables, add_valid_column_if_not_exists, insert_card
-
-create_tables()
-add_valid_column_if_not_exists()
-
-print("Cartas cargadas correctamente.")
--- migrate.py
+++
@@ -1,7 +0,0 @@
-from database import create_tables, add_valid_column_if_not_exists
-
-
-create_tables()
-add_valid_column_if_not_exists()
-
-print("Migration completed.")
